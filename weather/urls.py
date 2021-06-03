a281ca0090d07ad692b8b41b2a4dbfa83f5a5a86
abash shah <a4abash@gmail.com>
2021-06-03 10:16:48 +0545

    finally

--- weather/urls.py
+++ weather/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('foreign/', views.index,name='index'),  # foreign api
